9c8578a60158350f6f762ec27decb52454fdc000
sunnovation <poojapawandev96@gmail.com>
2024-06-29 22:27:39 +0530

    First Commit

---
+++ src/main/java/com/ai/EmaiApplication.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaiApplication.class, args);
+	}
+
+}
